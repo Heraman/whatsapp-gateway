edb0ed40c206d03643b48784eb02e04264db138f
Heraman <suryanizn43@gmail.com>
2026-04-20 23:25:23 +0000

    up

--- app/Models/Plans.php
+++ app/Models/Plans.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
-
+        
+#[Fillable(['name', 'price', 'duration', 'features'])]
 class Plans extends Model
 {
-    protected $fillable = ['name', 'price', 'duration', 'features'];
+    // protected $fillable = ['name', 'price', 'duration', 'features'];
 
     protected $casts = [
         'features' => 'array',
